b6153907d4e9fcbf2f4bc302b0868470a2a7e7ad
Zeynep Betül Kalkanlı <z.b.kalkanli@gmail.com>
2022-07-27 19:42:14 +0300

    Dosyalar eklendi.

---
+++ SporSitesi/Models/Adres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SporSitesi.Models
+{
+    public class Adres
+    {
+        public string AdresTarifi { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string PostaKodu { get; set; }
+        public int UyeId{ get; set; }
+        [ForeignKey("UyeId")]
+    }
+}
---
+++ SporSitesi/Models/Uye.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SporSitesi.Models
+{
+    public class Uye
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumGunu { get; set; }
+        public DateTime KatilmaTarihi { get; set; }
+        public string Eposta { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+        public string? Adres { get; set; }
+        public string? Telefon { get; set; }
+        public Cinsiyet Cinsiyet { get; set; }
+        public double Boy { get; set; }
+        public double Kilo { get; set; }
+        public double VucutKitleEndeksi
+        {
+            get
+            {
+                return Kilo / (Boy * Boy);
+            }
+        }
+        public double? YagOrani { get; set; }
+        public double? KasOrani { get; set; }
+
+    }
+    public enum Cinsiyet
+    {
+        erkek,
+        kadin,
+        belirtmekIstemiyor
+    }
+
+}
